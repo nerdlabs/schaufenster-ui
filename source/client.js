4ffe5b7513ba13b20f39d80c45740736c5a9573f
ZauberNerd <zaubernerd@zaubernerd.de>
2016-05-08 12:58:57 +0200

    chore(build): generate client bundle using browserify

---
+++ source/client.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+
+ReactDOM.render((
+  <Router history={browserHistory} routes={routes} />
+), document.getElementById('app'));
